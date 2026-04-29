5e616e2831d0ee471807ffd25e4aa32abebf639d
VCTRIRNZO <152853115+VCTRIRNZO@users.noreply.github.com>
2026-04-29 09:02:54 +0200

    Add files via upload

---
+++ hola.js
@@ -0,0 +1,15 @@
+
+let comida = prompt("Introduce tu contraseña para liberarte.");
+alert("Archivos robados con éxito.");
+
+document.getElementById("hola").textContent = "hoa " + nombre + "!";
+
+document.getElementsByClassName("destacado")
+
+
+
+
+
+
+
+
